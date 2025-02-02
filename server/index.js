7b279410e623ca951791c74da435e2d5816747b9
Kousick-krishna <kousickkrish1970@gmail.com>
2025-02-02 18:01:24 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ app.use(cors());
 const cors = require('cors');
 app.use(cors({ origin: 'https://www.geekspace.in' }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.geekspace.in');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
