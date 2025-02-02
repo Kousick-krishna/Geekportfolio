b9269554f3210da51a464c520d34169621f9a0a6
Kousick-krishna <kousickkrish1970@gmail.com>
2025-02-02 17:58:37 +0530

    first

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+const cors = require('cors');
+app.use(cors({ origin: 'https://www.geekspace.in' }));
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
